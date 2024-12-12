9cb89a03edcfd9bfac247310053f7db6a4277fa3
rokaia1692006 <rokaia.moh.a@gmail.com>
2024-12-12 04:13:14 +0200

    first_question

--- main.c
+++ main.c
@@ -6,6 +6,12 @@ typedef struct
 int Num;
 int Denom;
 } FRAC;
+FRAC Divide_fractions(FRAC n , FRAC m){
+    FRAC result;
+    result.Num = n.Num * m.Denom;
+    result.Denom = n.Denom * m.Num;
+    return result;
+}
 FRAC Multiply_fractions (FRAC n , FRAC m)
 {
 FRAC ans;
@@ -15,12 +21,15 @@ return ans;
 }
 void main()
 {
-FRAC N1, N2,N;
+FRAC N1, N2,N , D;
 N1.Num=3;
 N1.Denom=4;
 N2.Num=1;
 N2.Denom=2;
 N=Multiply_fractions(N1,N2);
+D = Divide_fractions(N1,N2);
+printf("result of division N1 by N2= %d / %d \n", D.Num,
+D.Denom);
 printf("result of subtract N1 to N2= %d / %d", N.Num,
 N.Denom);
 }
