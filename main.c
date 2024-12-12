c295ca709f0d0925acc4b027fffa34212b261aa9
rokaia1692006 <rokaia.moh.a@gmail.com>
2024-12-12 04:02:57 +0200

    STRUCTSSTART

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(){
-    return 0;
+typedef struct
+{
+int Num;
+int Denom;
+} FRAC;
+FRAC Multiply_fractions (FRAC n , FRAC m)
+{
+FRAC ans;
+ans.Num= n.Num * m.Num;
+ans.Denom = n.Denom * m.Denom;
+return ans;
+}
+void main()
+{
+FRAC N1, N2,N;
+N1.Num=3;
+N1.Denom=4;
+N2.Num=1;
+N2.Denom=2;
+N=Multiply_fractions(N1,N2);
+printf("result of subtract N1 to N2= %d / %d", N.Num,
+N.Denom);
 }
